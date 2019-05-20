2bff535a5ebd575bf46e60f04a102c74a845ecc8
willissa2121 <scott.willis.wsu@gmail.com>
2019-05-20 12:31:48 -0700

    updated sidebar

--- Website1/app.js
+++ Website1/app.js
@@ -1,1 +1,13 @@
 // alert()
+
+let openMenu = () => {
+  document.getElementById("sidebar-links").style.width="250px"
+  document.getElementById("main").style.marginLeft="250px"
+
+
+}
+
+let closeMenu = () => {
+document.getElementById("sidebar-links").style.width="0"
+document.getElementById("main").style.marginLeft="0"
+}
