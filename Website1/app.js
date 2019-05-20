11ce27ab5cf610d73dee980e452f56af572f0c12
willissa2121 <scott.willis.wsu@gmail.com>
2019-05-20 13:54:51 -0700

    side links and tab headers for resume

--- Website1/app.js
+++ Website1/app.js
@@ -3,6 +3,7 @@
 let openMenu = () => {
   document.getElementById("sidebar-links").style.width="250px"
   document.getElementById("main").style.marginLeft="250px"
+  
 
 
 }
@@ -11,3 +12,20 @@ let closeMenu = () => {
 document.getElementById("sidebar-links").style.width="0"
 document.getElementById("main").style.marginLeft="0"
 }
+
+let openContent = (idname) => {
+  let tabcontent = document.getElementsByClassName("tabcontent");
+  for (var i = 0; i < tabcontent.length; i++){
+    tabcontent[i].style.display="none"
+  }
+
+  let tablinks = document.getElementsByClassName("tablink");
+  for (var i = 0; i < tablinks; i++){
+    tablinks[i].style.backgroundColor=""
+  }
+  document.getElementById(idname).style.display="block";
+
+  
+
+}
+document.getElementById("default").click();
