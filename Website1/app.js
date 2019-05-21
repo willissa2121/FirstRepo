ccdf5d03635d6b2fd545f78059518fb73f706f14
Scott Willis <scott.willis.wsu@gmail.com>
2019-05-21 13:14:20 -0700

    working tablinks

--- Website1/app.js
+++ Website1/app.js
@@ -13,19 +13,27 @@ document.getElementById("sidebar-links").style.width="0"
 document.getElementById("main").style.marginLeft="0"
 }
 
-let openContent = (idname) => {
+let openContent = (id, btnId) => {
   let tabcontent = document.getElementsByClassName("tabcontent");
   for (var i = 0; i < tabcontent.length; i++){
     tabcontent[i].style.display="none"
   }
 
   let tablinks = document.getElementsByClassName("tablink");
-  for (var i = 0; i < tablinks; i++){
+  for (var i = 0; i < tablinks.length; i++){
+    console.log("working")
     tablinks[i].style.backgroundColor=""
+    tablinks[i].style.color=''
+    
+    
   }
-  document.getElementById(idname).style.display="block";
-
   
+  document.getElementById(id).style.display="block";
+  //This works, but the buttons will not unturn chosen color
+  document.getElementById(btnId).style.backgroundColor="teal"
+  document.getElementById(btnId).style.color="white"
+
+
 
 }
 document.getElementById("default").click();
